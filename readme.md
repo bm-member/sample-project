419d1410e1e547c2374eb99fdba0694686c14092
Thet Minn Htun <thetminnhtun92@gmail.com>
2019-11-11 20:58:02 +0630

    RESTful API

--- readme.md
+++ readme.md
@@ -33,3 +33,13 @@ crud
 API Resource
 Authentication with JSON Web Token(JWT)
 ```
+### Installation
+
+- git clone
+- copy .env.example .env
+- composer install
+- config database
+- php artisan migrate
+- php artisan db:seed
+- php artisan serve
+- done
--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,15 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/* Route::get('/post', 'API\PostController@index');
+Route::get('/post/{id}', 'API\PostController@show');
+Route::post('/post', 'API\PostController@store');
+Route::put('/post/{id}', 'API\PostController@update');
+Route::patch('/post/{id}', 'API\PostController@update');
+Route::delete('/post/{id}', 'API\PostController@destroy'); */
+
+Route::apiResource('post', 'API\PostController');
